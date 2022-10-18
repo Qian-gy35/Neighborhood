68418be8d2fd2da22b0f1a94233eab22ba3ed0ff
QianqianHong <77793476+QianqianHong@users.noreply.github.com>
2022-10-17 23:23:59 -0400

    Ultimate Commit

---
+++ util/yaml.go
@@ -0,0 +1,40 @@
+package util
+
+import (
+	"io/ioutil"
+	"path/filepath"
+)
+
+type ElasticsearchInfo struct {
+	Address  string `yaml:"address"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+}
+
+type GCSInfo struct {
+	Bucket string `yaml:"bucket"`
+}
+
+type TokenInfo struct {
+	Secret string `yaml:"secret"`
+}
+
+type ApplicationConfig struct {
+	ElasticsearchConfig *ElasticsearchInfo `yaml:"elasticsearch"`
+	GCSConfig           *GCSInfo           `yaml:"gcs"`
+	TokenConfig         *TokenInfo         `yaml:"token"`
+}
+
+func LoadApplicationConfig(configDir, configFile string) (*ApplicationConfig, error) {
+	content, err := ioutil.ReadFile(filepath.Join(configDir, configFile))
+	if err != nil {
+		return nil, err
+	}
+
+	var config ApplicationConfig
+	err = yaml.Unmarshal(content, &config)
+	if err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
