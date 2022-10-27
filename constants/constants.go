342af0299973ce15a06352d60665ffd553dc0333
Qian-gy35 <qjeanhong@gmail.com>
2022-10-26 20:50:01 -0400

    ultimate commit

---
+++ constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	USER_INDEX  = "user"
+	POST_INDEX  = "post"
+	ES_URL      = "http://10.128.0.69:9200"
+	ES_USERNAME = "qianqian"
+	ES_PASSWORD = "12345678"
+	GCS_BUCKET  = "qianqian-bucket"
+)
